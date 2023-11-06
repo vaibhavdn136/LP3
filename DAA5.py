e9d40ae885cc59adafda44bb5caf246a7f425f0f
Vaibhav Nanaware <104548588+vaibhavdn136@users.noreply.github.com>
2023-11-06 10:01:13 +0530

    Update DAA5.py

--- DAA5.py
+++ DAA5.py
@@ -1,37 +1,35 @@
 def n_queens(n):
     col = set()
-    posDiag=set() # (r+c)
-    negDiag=set() # (r-c)
+    posDiag = set()  # (r+c)
+    negDiag = set()  # (r-c)
 
-    res=[]
+    def count_solutions(board, r):
+        if r == n:
+            return 1
 
-    board = [["."]*n for i in range(n) ]
-    def backtrack(r):
-        if r==n:
-            copy = [" ".join(row) for row in board]
-            res.append(copy)
-            return
+        total_solutions = 0
 
         for c in range(n):
-            if c in col or (r+c) in posDiag or (r-c) in negDiag:
+            if c in col or (r + c) in posDiag or (r - c) in negDiag:
                 continue
 
             col.add(c)
-            posDiag.add(r+c)
-            negDiag.add(r-c)
-            board[r][c]="Q"
+            posDiag.add(r + c)
+            negDiag.add(r - c)
+            board[r][c] = "Q"
 
-            backtrack(r+1)
+            total_solutions += count_solutions(board, r + 1)
 
             col.remove(c)
-            posDiag.remove(r+c)
-            negDiag.remove(r-c)
-            board[r][c]="."
-    backtrack(0)
-    for sol in res:
-        for row in sol:
-            print(row)
-        print()
-    
-if __name__=="__main__":
-    n_queens(8)
+            posDiag.remove(r + c)
+            negDiag.remove(r - c)
+            board[r][c] = "."
+
+        return total_solutions
+
+    board = [["."]*n for i in range(n)]
+    return count_solutions(board, 0)
+
+if __name__ == "__main__":
+    num_solutions = n_queens(8)
+    print(f"Number of solutions for 8-Queens: {num_solutions}")
